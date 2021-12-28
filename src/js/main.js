4dbfaf4bf409dc52fa29ad7ed8d665a9773981e4
PaulaEPR <paulaperera@gmail.com>
2021-12-28 18:19:24 +0100

    feat: get data from API input search

--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,53 @@
 'use strict';
 
-/* --- Funciones --- */
+/* --- Global Variables --- */
 
-//Crea la etiqueta HTML
+const input = document.querySelector('.js-input');
+const button = document.querySelector('.js-submit');
+const main = document.querySelector('.js-main');
+
+/* --- Functions --- */
+
+//Create HTML tag
 function createTag(tag, className) {
   const newTag = document.createElement(tag);
   newTag.classList.add(className);
   return newTag;
 }
 
-//Crear un artículo con título e imagen de fondo
+//Create an article with title and background image
 function createBkgImgArticle(title, imgUrl) {
   const thisMother = createTag('article', 'js-article');
   const thisChild = createTag('h5', 'js-article-title');
-  const newtitle = document.createTextNode(title);
-  thisChild.appendChild(newtitle);
+  const newTitle = document.createTextNode(title);
+  thisChild.appendChild(newTitle);
   thisMother.appendChild(thisChild);
   thisMother.style.background = `url('${imgUrl}') center bottom / cover no-repeat`;
   return thisMother;
 }
 
-fetch('https://api.jikan.moe/v3/search/anime?q=naruto')
-  .then((response) => response.json())
-  .then((data) => {
-    const result = data.results;
-    for (const item of result) {
-      const title = item.title;
-      const imgUrl = item.image_url;
-      const article = createBkgImgArticle(title, imgUrl);
-      const mother = document.querySelector('.main');
-      mother.appendChild(article)
-    }
-  });
+//Listen to events in the search button
+function handleChangeInput(event) {
+  event.preventDefault();
+  main.innerHTML = '';
+  getData();
+}
+
+button.addEventListener('click', handleChangeInput);
+
+/* --- Obtener datos de la API --- */
+
+function getData() {
+  const apiURL = `https://api.jikan.moe/v3/search/anime?q=${input.value}`;
+  fetch(apiURL)
+    .then((response) => response.json())
+    .then((data) => {
+      const result = data.results;
+      for (const item of result) {
+        const title = item.title;
+        const imgUrl = item.image_url;
+        const article = createBkgImgArticle(title, imgUrl);
+        main.appendChild(article);
+      }
+    });
+}
